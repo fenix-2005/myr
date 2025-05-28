ee84d4a4e7270fa310f1959364822b8670670cbb
javier Guzman <javi.guzmanm@duocuc.cl>
2025-05-28 14:34:30 -0400

    commit

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 
-list1 = []
+listn = []
 
 while True:
 
     print ("-----Menu de notas-----")
-    print ("1- ")
+    print ("1- Notas")
+    print ("2- Promedio de notas")
+    choice = int(input(""))
+
+    if choice == 1:
+        times = int(input("Ingrese cuantas notas va a calcular: "))
+
+        for i in times:
+            print (f"Ingrese la nota {i+1}")
+            note = int(input(""))
+
+    elif choice == 2:
+        print
+    else:
+        print ("Datos ingresados no validos por favor reintente")
